219469683434be11f8d6fb8fd7cb9c59c20a0f00
plnvsln <mr.mightypo@gmail.com>
2023-01-08 09:02:34 +0000

    added footer

--- src/App.js
+++ src/App.js
@@ -4,16 +4,29 @@ import Form from "./Form";
 function App() {
   return (
     <div className="App">
-      <Form />
-      <h1>New York</h1>
-      <ul>
-        <li>Last updated: Tuesday 10:00</li>
-        <li>Cloudy</li>
-        <li>Humidity: 80%</li>
-        <li>Wind: 10 km/h</li>
-      </ul>
+      <div className="content">
+        <Form />
+        <h1>New York</h1>
+        <ul>
+          <li>Last updated: Tuesday 10:00</li>
+          <li>Cloudy</li>
+          <li>Humidity: 80%</li>
+          <li>Wind: 10 km/h</li>
+        </ul>
 
-      <div className="temp">15</div>
+        <div className="temp">15</div>
+      </div>
+      <div className="footer">
+        <a
+          href="https://github.com/plnvsln/weather-react"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {" "}
+          Open-sourced{" "}
+        </a>
+        by Polina Chernii
+      </div>
     </div>
   );
 }
