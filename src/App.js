f66ef0b025cce42822423283104ed82393a9183a
plnvsln <mr.mightypo@gmail.com>
2023-01-15 18:43:29 +0000

    created new component with formatted date

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Form from "./Form";
 import axios from "axios";
 import React, { useState } from "react";
+import FriendlyDate from "./FriendlyDate";
 
 export default function App() {
   const [weatherData, setWeatherData] = useState({ ready: false });
@@ -17,7 +18,8 @@ export default function App() {
       city: responce.data.city,
       description: responce.data.condition.description,
       wind: responce.data.wind.speed,
-      date: `Tuesday 10:00`,
+      date: new Date(responce.data.time * 1000),
+      icon: responce.data.condition.icon_url,
     });
   }
 
@@ -26,30 +28,27 @@ export default function App() {
       <div className="App">
         <Form />
         <h1>{weatherData.city}</h1>
-        <h5>{weatherData.date}</h5>
+        <h5>
+          <FriendlyDate date={weatherData.date} />
+        </h5>
         <h6 className="text-capitalize">{weatherData.description} </h6>
         <div className="container">
           <div className="row">
-            <div className="col-6">
-              {" "}
-              <img
-                src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-                alt="sunny"
-              ></img>
-              <div className="temp">
+            <div className="col-6 ">
+              <img src={weatherData.icon} alt="sunny"></img>
+              <span className="temp">
                 {Math.round(weatherData.temperature)} °C
-              </div>
+              </span>
             </div>
             <div className="col-6">
               <ul>
                 <li>Humidity: {weatherData.humidity}%</li>
                 <li>Wind: {weatherData.wind} m/h</li>
-                <li>Feels like: {Math.round(weatherData.feels_like)} °C</li>
+                <li>Feels like: {Math.round(weatherData.feels_like)}°C</li>
               </ul>
             </div>
           </div>
         </div>
-
         <div className="footer">
           <a
             href="https://github.com/plnvsln/weather-react"
